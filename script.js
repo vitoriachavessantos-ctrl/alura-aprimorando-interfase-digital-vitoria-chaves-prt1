b07c4727789cbcedadda36f88450ad5adbf612ca
vitoriachavessantos-ctrl <vitoria.chaves.santos@escola.pr.gov.br>
2026-08-26 13:23:37 -0300

    Add button click functionality to toggle likes

---
+++ script.js
@@ -0,0 +1,36 @@
+const botoes = document.querySelectorAll(".botoes button");
+
+
+botoes.forEach(function (botao) {
+
+    let curtiu = false;
+
+
+    botao.addEventListener("click", function () {
+
+        const numeroSpan = botao.querySelector("span");
+
+        const quantidadeAtual = Number(numeroSpan.textContent);
+
+
+        if (curtiu === false) {
+
+            numeroSpan.textContent = quantidadeAtual + 1;
+
+            curtiu = true;
+
+            botao.classList.add("curtido");
+
+        } else {
+
+            numeroSpan.textContent = quantidadeAtual - 1;
+
+            curtiu = false;
+
+            botao.classList.remove("curtido");
+
+        }
+
+    });
+
+});
